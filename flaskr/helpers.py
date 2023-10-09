94315b7e1c36d874c0befe592ffe94a7ce7c35ef
danzel ong <danzelongwj01@gmail.com>
2023-10-09 22:10:39 +0800

    feat (helpers): implemented 'create playlist feature' logic"

--- flaskr/helpers.py
+++ flaskr/helpers.py
@@ -31,8 +31,13 @@ def create_spotify_oauth(user_id=False):
 
 
 def get_recommended_tracks(spotify_API, count):
+    recommended_tracks , top_genre = None, None
+
     # get the user's recent played tracks
     recent_tracks = spotify_API.current_user_recently_played(limit=count)['items']
+
+    if not recent_tracks:
+        return recommended_tracks, top_genre
 
     artist_ids = [track['track']['artists'][0]['id'] for track in recent_tracks]
     track_ids = [track['track']['id'] for track in recent_tracks]
@@ -67,5 +72,12 @@ def get_recommended_tracks(spotify_API, count):
     return recommended_tracks, top_genre
 
 
-def create_playlist():
-    pass
+def create_playlist(playlist_name, tracks, user_id):
+    cache_file = CacheFileHandler(cache_path=os.path.join(CACHE_FOLDER, f".cache-{user_id}"))
+    token = cache_file.get_cached_token()
+    spotify_API = Spotify(auth=token['access_token'])
+
+    # create playlist
+    playlist = spotify_API.user_playlist_create(user=user_id, name=playlist_name)
+    spotify_API.user_playlist_add_tracks(user_id, playlist['id'], tracks)
+    return playlist['name']
